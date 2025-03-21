72918a392399318ae5bc738f96da5c5b9e17121b
NeKordebalet <nekordebalet2@tutamail.com>
2025-03-21 13:18:51 +0300

    fcghkjn

--- poker.py
+++ poker.py
@@ -13,7 +13,25 @@ class TexasHoldem:
     def win_chance(self):
         return f"Шансы на победу: {self.calculate_win_chance()}%"
     def calculate_win_chance(self):
-        return 50 
+        total_cards = 52 #52 всего
+        player_cards = 2  # 2 карты в Texas
+        remaining_cards = total_cards - player_cards  # Оставшиеся карты в колоде
+        winning_combinations = 0
+        total_combinations = 0
+
+        # Перебираем возможные карты, которые могут проап руку
+        for _ in range(10000):  
+            random_card_value = random.choice(['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2'])
+            random_card_suit = random.choice(['♠', '♥', '♦', '♣'])
+            total_combinations += 1
+            if random_card_value in [self.first_card_value, self.second_card_value]:
+                winning_combinations += 1
+        #вероятность
+        if total_combinations > 0:
+            win_chance = (winning_combinations / total_combinations) * 100
+        else:
+            win_chance = 0
+        return round(win_chance, 2)
 class OmahaHoldem(TexasHoldem):
     def __init__(self, first_card_value, first_card_suit, second_card_value, second_card_suit,
                  third_card_value, third_card_suit, forth_card_value, forth_card_suit):
@@ -35,10 +53,8 @@ class OmahaHoldem(TexasHoldem):
         total_cards = 52 #52 карты всего
         player_cards = 4  # 4 карты в Omaha
         remaining_cards = total_cards - player_cards  # Оставшиеся карты в колоде
-
         winning_combinations = 0
         total_combinations = 0
-
         # Перебираем карты, которые могут проапгрейдить руку
         for _ in range(10000):
             random_card_value = random.choice(['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2'])
@@ -47,7 +63,6 @@ class OmahaHoldem(TexasHoldem):
             if random_card_value in [self.first_card_value, self.second_card_value,
                                       self.third_card_value, self.forth_card_value]:
                 winning_combinations += 1
-
         # считаю вероятность
         if total_combinations > 0:
             win_chance = (winning_combinations / total_combinations) * 100
@@ -58,7 +73,6 @@ if __name__ == "__main__":
     texas_hand = TexasHoldem("5", "♠", "6", "♥")
     print(texas_hand.pocket_info())
     print(texas_hand.win_chance())
-
     omaha_hand = OmahaHoldem("10", "♠", "9", "♥", "Q", "♦", "J", "♣")
     print(omaha_hand.pocket_info())
     print(omaha_hand.win_chance())
